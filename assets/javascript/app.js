d7addab0da156dec84b2e25d62e87eaf89b089b7
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-05-25 16:36:47 -0700

    updated questions and named additional functions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,17 @@ questionCsv =
     [
         "Who played Andy Dufresne in the ShawShank Redemption?, Morgan Freeman, Tim Robbins, Robert Dinero, William Sadler, Correct Answer Tim Robbins",
         "The Godfather was released in what year?, 1972, 1971, 1973, 1974, Correct Answer 1972",
-        ""
+        "Who was the best Batman of all time?, Michael Keaton, Val Kilmer, Christian Bale, George Clooney, Correct Answer Michael Keaton",
+        "Johnny Utah was played by whom?, Keanu Reeves, Patrick Swayze, Eddie Murphy, Gary Busey, Correct Answer Keanu Reeves",
+        "Lethal Weapon was released in what year?, 1987, 1988, 1985, 1986, Correct Answer 1987",
+        "1987 buddy cop movie staring both Tom Hanks and Dan Akroyd?, Spies Like Us, Dragnet, Turner & Hooch, Big, Correct Answer Dragnet",
+        "Axel Foley was played by which male actor?, Chevy Chase, Martin Lawrence, Eddie Murphy, Mel Gibson, Correct Answer Eddie Murphy",
+        "Which character said the following? Back in '82 I used to be able to throw a pigskin a quarter mile, Kipp, Napolean, Uncle Rico, Jonah Hill, Correct Answer Uncle Rico?",
+        "Who played the female lead in Memoirs of an Invisible Man?, Daryl Hannah, Jane Fonda, Debra Winger, Sissy Spacek",
+        "Who played the female lead in True Lies?, Sigourney Weaver, Diane Keaton, Jamie Lee Curtis",
+
+
+
         //and so on
     ];
 
@@ -48,18 +58,22 @@ function updateQuestion(newQuestion) {
 
 }
 
+//this function will check for the correct answer
 function checkAnswers() {
 
 }
 
+//this function will launch the timer
 function timer() {
 
 }
 
+//this function will keep track of score
 function scoreCalculator() {
 
 }
 
+//this function will restart the game after completion.  
 function gameReset() {
 
 }
