8a1568712684797f87de85c36611c70af7911e90
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-05-24 18:09:45 -0700

    adding button. and initial javascript functions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,21 @@
 /**
  * Created by ryanhoyda on 5/22/17.
  */
+
+
+question = {
+    question: 'Some question',
+    one : 'Answer one',
+    two : 'Answer two',
+    three : 'Answer three',
+    four: 'Answer four',
+    answer: 'one'
+}
+
+function updateQuestion(newQuestion){
+    $('#question1').text(newQuestion.question);
+    $('#a1').text(newQuestion.one);
+    $('#a2').text(newQuestion.two);
+    $('#a3').text(newQuestion.three);
+    $('#a4').text(newQuestion.four);
+}
